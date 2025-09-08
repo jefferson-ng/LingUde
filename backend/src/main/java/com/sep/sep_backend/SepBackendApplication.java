4b3cc9e6ff32f874e35e84db8fbabeb9b1262562
Alexander Korn <alexander.korn@uni-due.de>
2025-09-08 13:39:22 +0200

    Initial commit

---
+++ backend/src/main/java/com/sep/sep_backend/SepBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sep.sep_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SepBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sep/sep_backend/SepBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sep.sep_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SepBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
