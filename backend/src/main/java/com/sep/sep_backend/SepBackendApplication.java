0808b3ca46b9c4bacdc31cbca4fd9b78319c6786
Hamit Aydin <hamit.aydin@stud.uni-due.de>
2025-11-13 10:12:57 +0100

    dummy daten einstz für später

--- backend/src/main/java/com/sep/sep_backend/SepBackendApplication.java
+++ backend/src/main/java/com/sep/sep_backend/SepBackendApplication.java
@@ -1,13 +1,27 @@
 package com.sep.sep_backend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.sep.sep_backend.user.repository.UserRepository;
 
 @SpringBootApplication
 public class SepBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SepBackendApplication.class, args);
+	
+	}
+ @Bean
+	CommandLineRunner run(UserRepository userRepository) {
+		return args -> {
+			System.out.println("Hello, World!");
+		};
 	}
 
+
 }
+
+
