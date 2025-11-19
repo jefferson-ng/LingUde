9150b6509b81694b4d276bb2dac28008d0e04b2c
Hamit Aydin <hamit.aydin@stud.uni-due.de>
2025-11-19 16:34:46 +0100

    endstand fix kommunikation

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,17 +1,20 @@
 import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideTransloco } from '@jsverse/transloco';
 
 import { routes } from './app.routes';
 import { TranslocoHttpLoader } from './transloco-loader';
+import { AuthInterceptor } from './services/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([AuthInterceptor])
+    ),
     provideTransloco({
       config: {
         availableLangs: ['de', 'en'],
